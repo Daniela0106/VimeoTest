9eddb6299ab0afb8e5a32aa55c8acbd037e0255d
Daniela0106 <daniela.barrientos.grisales@gmail.com>
2016-06-20 18:14:25 -0500

    Still not fully functional UI...

--- VimeoTest/src/app/index.route.js
+++ VimeoTest/src/app/index.route.js
@@ -1,55 +1,30 @@
-var vimeoProject = angular.module('vimeoProject', ['ui.router'])
+var app = angular.module('vimeo1', ['ui.router']);
 
-(function() {
-  'use strict';
-  var vm = this;
-  vm.awesomeThings = [];
+app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/');
 
-  angular
-    .module('vimeoTest')
-    .config(routerConfig);
+  /**@ngInject**/
+  $stateProvider
+    .state('main', {
+      url:'/main',
+      templateUrl: 'main.html',
+      controller: 'main'
+    })
+    .state('details', {
+      url:'/details',
+      templateUrl: 'video/details.html',
+      controller: 'details'
+    })
+    .$stateParams('',{
+      url:'/main',
+      templateUrl: 'main.html',
+      controller: 'main'
+    } )
 
-  /** @ngInject */
-  function routerConfig($stateProvider, $urlRouterProvider) {
-    $stateProvider
-      .state('home', {
-        url: '/',
-        templateUrl: 'app/main/main.html',
-        controller: 'MainController',
-        controllerAs: 'main'
-      });
-    
-    
-
-    $urlRouterProvider.otherwise('/');
-  }
-
-})();
+}]);
 
 
-/*
-// app.js
- var routerApp = angular.module('routerApp', ['ui.router']);
 
- routerApp.config(function($stateProvider, $urlRouterProvider) {
-
- $urlRouterProvider.otherwise('/home');
-
- $stateProvider
-
- // HOME STATES AND NESTED VIEWS ========================================
- .state('home', {
- url: '/home',
- templateUrl: 'partial-home.html'
- })
-
- // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
- .state('about', {
- // we'll get to this in a bit
- });
-
- });
-* */
 
 
 
